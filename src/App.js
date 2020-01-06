08ca64260c2fd5e34fcdad85dc82bb635f0750f8
Dawn Ribeiro <wildorchid1006@yahoo.com>
2020-01-06 15:42:26 -0500

    working on axios call

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
 import HelloWorld from './components/HelloWorld'
+import ActiveCalls from './components/ActiveCalls'
 
 class App extends Component {
   render() {
-    return <HelloWorld />
+    return <ActiveCalls />
   }
 }
 
---
+++ src/components/ActiveCalls.jsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+export default function ActiveCalls() {
+  const [calls, setCalls] = useState({})
+
+  useEffect(() => {
+    axios
+      .get('https://dev.somahub.io/api/1/calls', {
+        headers: {
+          Authorization: 'Basic am9zaEBzb21hZ2xvYmFsLmNvbTpwYXNzd29yZA==',
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      })
+      .then(resp => {
+        setCalls(resp.data)
+        console.log(resp.data)
+      })
+  })
+  return <p>HELLO</p>
+}
