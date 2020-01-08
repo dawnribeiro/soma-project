ce2a9f6dc64fed86e842e029ed495464defac144
Dawn Ribeiro <wildorchid1006@yahoo.com>
2020-01-08 17:05:47 -0500

    working on filter

--- src/components/ActiveCalls.jsx
+++ src/components/ActiveCalls.jsx
@@ -20,41 +20,48 @@ export default function ActiveCalls() {
         return axios.get('/api/1/callTypes').then(resp => {
           setCallTypes(resp.data)
           console.log('call types', resp.data)
+          let myCallType = callTypes.filter(
+            types => types.Key === calls.CallType
+          )
+          console.log(myCallType)
         })
       })
   }, [])
+
   return (
     <section>
       <div>
         <ul className="list">
           {calls.map(call => {
-            return (
-              <li key={call.Id} className="card">
-                <div className="first">
-                  {/* {callTypes.map(type => {
-                    callTypes.filter(type.Key === call.CallType)
-                    return <p>{type.Name}</p>
-                  })} */}
-                  <p className="title">{call.CallType}</p>
-                  <p className="address">
-                    {call.Location.AddNum} {call.Location.StPreDir}{' '}
-                    {call.Location.StName} {call.Location.StType}{' '}
-                    {call.Location.StDir}, {call.Location.City},{' '}
-                    {call.Location.Region}, {call.Location.Postal}
-                  </p>
-                </div>
-                <div className="bottom-group">
-                  <div className="second">
-                    <p>{call.Status}</p>
-                    <Moment fromNow>{call.CreatedAt}</Moment>
+            {
+              // let myCallType = callTypes.filter(
+              //   types => types.Key === call.CallType
+              // )
+              return (
+                <li key={call.Id} className="card">
+                  <div className="first">
+                    {/* <p>{myCallType[0].Name}</p> */}
+                    <p className="title">{call.CallType}</p>
+                    <p className="address">
+                      {call.Location.AddNum} {call.Location.StPreDir}{' '}
+                      {call.Location.StName} {call.Location.StType}{' '}
+                      {call.Location.StDir}, {call.Location.City},{' '}
+                      {call.Location.Region}, {call.Location.Postal}
+                    </p>
                   </div>
-                  <div className="third">
-                    <p>#{call.CallNumber.slice(-4)}</p>
-                    <p>Priority: {call.Priority}</p>
+                  <div className="bottom-group">
+                    <div className="second">
+                      <p>{call.Status}</p>
+                      <Moment fromNow>{call.CreatedAt}</Moment>
+                    </div>
+                    <div className="third">
+                      <p>#{call.CallNumber.slice(-4)}</p>
+                      <p>Priority: {call.Priority}</p>
+                    </div>
                   </div>
-                </div>
-              </li>
-            )
+                </li>
+              )
+            }
           })}
         </ul>
       </div>
